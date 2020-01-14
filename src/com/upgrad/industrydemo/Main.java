8fa6af920071f46d061efbd74bba924f95c70494
Pritesh Kumar <pritesh.kumar2@gmail.com>
2020-01-14 13:18:20 +0530

    add initial imp of industrydemo

---
+++ src/com/upgrad/industrydemo/Main.java
@@ -0,0 +1,8 @@
+package com.upgrad.industrydemo;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/com/upgrad/industrydemo/WebObject.java
@@ -0,0 +1,8 @@
+package com.upgrad.industrydemo;
+
+/** Interface for all the web facing objects **/
+public interface WebObject {
+  
+  /** Abstract method declaration. No need for access modifier, by default public. **/
+  String getUUID();
+}
